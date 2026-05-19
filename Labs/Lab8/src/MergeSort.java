ca5df2a9a4fd1ec1f3815611e08e67d563d5bf09
Alex Melis <overshoot69@gmail.com>
2026-05-19 14:19:16 -0700

    Finished lab8

---
+++ Labs/Lab8/src/MergeSort.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] array = getUserInput();
+
+        System.out.println("Submitted array: " + Arrays.toString(array));
+        array = mergeSort(array);
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+
+    public static int[] mergeSort(int[] array) {
+        if (array.length <= 1) return array;
+
+        int partition = array.length / 2;
+
+        int[] leftPart = Arrays.copyOfRange(array, 0, partition);
+        int[] rightPart = Arrays.copyOfRange(array, partition, array.length);
+
+        return merge(mergeSort(leftPart), mergeSort(rightPart));
+    }
+
+    public static int[] merge(int[] leftPart, int[] rightPart) {
+        int rightPointer = 0, leftPointer = 0, totalPointer;
+        int[] total = new int[leftPart.length + rightPart.length];
+
+        for (totalPointer = 0;
+             rightPointer < rightPart.length && leftPointer < leftPart.length;
+             totalPointer++) {
+            if (leftPart[leftPointer] < rightPart[rightPointer]) {
+                total[totalPointer] = leftPart[leftPointer];
+                leftPointer++;
+            } else {
+                total[totalPointer] = rightPart[rightPointer];
+                rightPointer++;
+            }
+        }
+
+        while (leftPointer < leftPart.length) {
+            total[totalPointer] = leftPart[leftPointer];
+            leftPointer++;
+            totalPointer++;
+        }
+
+        while (rightPointer < rightPart.length) {
+            total[totalPointer] = rightPart[rightPointer];
+            rightPointer++;
+            totalPointer++;
+        }
+
+        return total;
+    }
+
+    public static int[] getUserInput() {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter a string of numbers separated by spaces: " );
+        String[] inputInts = input.nextLine().split(" ");
+
+        int[] realInts = new int[inputInts.length];
+        for (int i = 0; i < inputInts.length; i++) {
+            try {
+                realInts[i] = Integer.parseInt(inputInts[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input string!");
+            }
+        }
+
+        return realInts;
+    }
+}
+
